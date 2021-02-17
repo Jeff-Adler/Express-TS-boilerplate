73b201542122c4b8fc9d6b1eaef94efae6b09c04
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-16 20:45:51 -0500

    fully cleaned up cleanJWT

--- src/middleware/checkJwt.ts
+++ src/middleware/checkJwt.ts
@@ -5,25 +5,27 @@ export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
   let jwtPayload;
 
   try {
-    const token: string = req.header('Authorization').replace('Bearer ', '');
+    if (!req.header) throw new Error();
+
+    const token: string = req.header('Authorization')!.replace('Bearer ', '');
     jwtPayload = <any>jwt.verify(token, process.env.JWT_SECRET as jwt.Secret);
     //res.locals is the conventional way to add fields to response in intermediary middleware
     res.locals.jwtPayload = jwtPayload;
+
+    // Send a new token on every request
+    const { userId, email } = jwtPayload;
+    const newToken = jwt.sign(
+      { userId, email },
+      process.env.JWT_SECRET as jwt.Secret,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      }
+    );
+    res.setHeader('token', newToken);
+
+    next();
   } catch (error) {
     res.status(401).send();
     return;
   }
-
-  // Send a new token on every request
-  const { userId, email } = jwtPayload;
-  const newToken = jwt.sign(
-    { userId, email },
-    process.env.JWT_SECRET as jwt.Secret,
-    {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    }
-  );
-  res.setHeader('token', newToken);
-
-  next();
 };
--- src/middleware/checkRole.ts
+++ src/middleware/checkRole.ts
@@ -5,15 +5,14 @@ import { User } from '../components/user/model';
 
 export const checkRole = (roles: Array<string>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    //Get the user ID from previous middleware
+    // Get the user ID from previous middleware
     const id = res.locals.jwtPayload.userId;
 
-    //Get user role from the database
     const userRepository: Repository<User> = getRepository(User);
     let user: User;
     try {
       user = await userRepository.findOneOrFail(id);
-      //Check if array of authorized roles includes the user's role
+      // Check if array of authorized roles includes user's role
       if (roles.indexOf(user.role) > -1) next();
       else res.status(401).send();
     } catch (id) {
