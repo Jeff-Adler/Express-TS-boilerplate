5a9cefefc35c8942df1dba5d5e497670d0c0c5c4
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-19 17:42:26 -0500

    fixed checkRole

--- src/middleware/checkRole.ts
+++ src/middleware/checkRole.ts
@@ -3,9 +3,10 @@ import { getRepository, Repository } from 'typeorm';
 
 import { User } from '../components/user/model';
 import { roleValidator } from '../components/user/utils/RoleValidator';
+import { UserRoles } from '../components/user/utils/UserRoles';
 
 // checkRole depends on checkJwt to extract user from JWT
-export const checkRole = (roles: Array<string>) => {
+export const checkRole = (roles: Array<keyof UserRoles>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const id = res.locals.jwtPayload.userId;
 
@@ -13,7 +14,7 @@ export const checkRole = (roles: Array<string>) => {
 
     try {
       const user: User = await userRepository.findOneOrFail(id);
-      if (roleValidator(user.role)) next();
+      if (roles.includes(user.role)) next();
       else res.status(401).send();
     } catch (id) {
       res.status(401).send();
