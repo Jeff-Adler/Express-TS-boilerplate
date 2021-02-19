c5deff8df3938d19bab15901d08f8bdc06d8c185
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-19 16:37:55 -0500

    cleaned up checkRole formatting

--- src/middleware/checkRole.ts
+++ src/middleware/checkRole.ts
@@ -7,13 +7,12 @@ import { roleValidator } from '../components/user/utils/RoleValidator';
 // checkRole depends on checkJwt to extract user from JWT
 export const checkRole = (roles: Array<string>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    // Get the user ID from previous middleware.
     const id = res.locals.jwtPayload.userId;
 
     const userRepository: Repository<User> = getRepository(User);
-    let user: User;
+
     try {
-      user = await userRepository.findOneOrFail(id);
+      const user: User = await userRepository.findOneOrFail(id);
       if (roleValidator(user.role)) next();
       else res.status(401).send();
     } catch (id) {
