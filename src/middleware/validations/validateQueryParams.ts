70afe43dfc3185e85f516049f85d3be02c0ef4fa
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-28 11:58:56 -0500

    debugged issue where headers sent after invalid request

--- src/middleware/validations/validateQueryParams.ts
+++ src/middleware/validations/validateQueryParams.ts
@@ -3,7 +3,10 @@ import { Request, Response, NextFunction } from 'express';
 export const validateQueryParams = (req: Request, res: Response, next: NextFunction): void => {
   if (req.query) {
     for (const key in req.query) {
-      if (typeof req.query[key] !== 'string') res.status(400).send('Request query is invalid');
+      if (typeof req.query[key] !== 'string') {
+        res.status(400).send('Request query is invalid');
+        return;
+      }
     }
   }
   next();
