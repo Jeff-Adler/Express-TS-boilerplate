a830fa34c6f989cdbdf71a640e37e947ccf5272f
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-28 19:40:49 -0500

    setup beforeAll methods in app.test.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,19 +1,34 @@
 import request from 'supertest';
 import express from 'express';
 import { App } from './app';
-import { createConnection } from 'typeorm';
+import { createConnection, Connection } from 'typeorm';
 
 describe('Test Server', () => {
-  it('Request / should return "Server is running!"', async () => {
-    console.log('Initializing ORM connection...');
-    await createConnection();
+  const app: express.Application = new App().app;
+  let connection: Connection;
+  beforeAll(async () => {
+    // change these options
+    connection = await createConnection({
+      name: 'default',
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+    });
 
-    const app: express.Application = new App().app;
     const PORT = process.env.PORT || 8080;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
+  });
 
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('Request / should return "Server is running!"', async () => {
     const result = await request(app).get('/').send();
 
     expect(result.status).toBe(200);
