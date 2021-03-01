c212cafdb17463716802ab12aebe4fd9e10993c9
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-28 19:25:32 -0500

    wrote first successful test, but need to figure out how to run server

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,11 +1,22 @@
 import request from 'supertest';
+import express from 'express';
 import { App } from './app';
+import { createConnection } from 'typeorm';
 
 describe('Test Server', () => {
   it('Request / should return "Server is running!"', async () => {
-    const result = await request(App).get('/').send();
+    console.log('Initializing ORM connection...');
+    await createConnection();
+
+    const app: express.Application = new App().app;
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    const result = await request(app).get('/').send();
 
     expect(result.status).toBe(200);
-    expect(result.body.data).toBe('Server is running!');
+    expect(result.text).toBe('Server is running!');
   });
 });
