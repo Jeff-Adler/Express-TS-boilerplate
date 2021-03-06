1ea3b0f9a3ae751bded479f6b93158c5c96f2075
Jeff Adler <JeffM.Adler@gmail.com>
2021-03-06 16:40:04 -0500

    auth tests failing

--- src/components/auth/tests/auth.test.ts
+++ src/components/auth/tests/auth.test.ts
@@ -12,8 +12,15 @@ describe('Testing Auth component', () => {
   });
 
   describe('POST /login', () => {
-    test('responds with 200 status if valid credentials are passed');
+    test.todo('responds with 200 status if valid credentials are passed');
 
-    test('responds with 401 status if invalid credentials are passed');
+    test('responds with 401 status if invalid credentials are passed', async () => {
+      const result = await factory.app.post('/login').send({
+        email: 'invalidemail@admin.com',
+        password: 'invalid_password',
+      });
+
+      expect(result.status).toBe(401);
+    });
   });
 });
