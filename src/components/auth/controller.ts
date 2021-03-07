32d0e1e017eea4cb070b33e67f49d8d86749b0a2
Jeff Adler <JeffM.Adler@gmail.com>
2021-03-07 10:17:04 -0500

    fixed 400 auth tests{

--- src/components/auth/controller.ts
+++ src/components/auth/controller.ts
@@ -8,7 +8,7 @@ export class AuthController {
   public async login(req: Request, res: Response) {
     let { email, password } = req.body;
     if (!(email && password)) {
-      res.status(400).send('Request body lacked fields: email and/or password');
+      return res.status(400).send('Request body lacked fields: email and/or password');
     }
 
     const userRepository: Repository<User> = getConnection(process.env.CONNECTION_TYPE).getRepository(User);
@@ -17,20 +17,15 @@ export class AuthController {
       user = await userRepository.findOneOrFail({ where: { email } });
 
       if (!user.checkIfUnencryptedPasswordIsValid(password)) {
-        res.status(401).send('Invalid password');
-        return;
-      } else {
-        const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET as jwt.Secret, {
-          expiresIn: process.env.JWT_EXPIRES_IN,
-        });
-
-        res.status(200).send({ token });
-        return;
+        return res.status(401).send('Invalid password');
       }
+      const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET as jwt.Secret, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+
+      return res.status(200).send({ token });
     } catch (error) {
-      res.status(401).send(`User with email address "${email}" not found`);
-      return;
-      // return res.status(401).json({ error: `User with email address "${email}" not found` });
+      return res.status(401).send(`User with email address "${email}" not found`);
     }
   }
 }
--- src/components/auth/tests/auth.test.ts
+++ src/components/auth/tests/auth.test.ts
@@ -1,4 +1,5 @@
 import { TestFactory } from '../../../utils/tests/factory';
+import { doesNotThrow } from 'assert';
 
 describe('Testing Auth component', () => {
   let factory: TestFactory = new TestFactory();
@@ -14,38 +15,35 @@ describe('Testing Auth component', () => {
   describe('POST /login', () => {
     test.todo('responds with 200 status if valid credentials are passed');
 
-    test('responds with 400 status if login request body does not include fields: email and/or password', (done) => {
-      factory.app.post('/auth/login').send({}).set('Accept', 'application/json').expect(400, done);
+    test('responds with 400 status if login request body does not include fields: email and/or password', async (done) => {
+      const result = await factory.app.post('/auth/login').send({});
 
-      // expect(result.status).toBe(400);
-      // expect(result.text).toBe('Request body lacked fields: email and/or password');
+      expect(result.status).toBe(400);
+      expect(result.text).toBe('Request body lacked fields: email and/or password');
+      done();
     });
 
-    // test('responds with 401 status if invalid password is passed', (done) => {
-    //   factory.app
-    //     .post('/auth/login')
-    //     .send({
-    //       email: 'admin@admin.com',
-    //       password: 'invalid_password',
-    //     })
-    //     .set('Accept', 'application/json')
-    //     .expect(401, done);
+    test('responds with 401 status if invalid password is passed', async (done) => {
+      const result = await factory.app.post('/auth/login').send({
+        email: 'admin@admin.com',
+        password: 'invalid_password',
+      });
 
-    //   // expect(result.status).toBe(401);
-    //   // Uncomment once database seeding is setup:
-    //   // expect(result.text).toBe('Invalid password');
-    // });
+      expect(result.status).toBe(401);
+      done();
+      // Uncomment once database seeding is setup:
+      // expect(result.text).toBe('Invalid password');
+    });
 
-    // test('responds with 401 status if invalid email is passed', async () => {
-    //   const result = await factory.app.post('/auth/login').send({
-    //     email: 'invalidemailaddress@admin.com',
-    //     password: 'admin_password',
-    //   });
+    test('responds with 401 status if invalid email is passed', async (done) => {
+      const result = await factory.app.post('/auth/login').send({
+        email: 'invalidemailaddress@admin.com',
+        password: 'admin_password',
+      });
 
-    //   // console.log(JSON.stringify(result));
-
-    //   expect(result.status).toBe(401);
-    //   // expect(result.text).toBe('User with email address "invalidemailaddress@admin.com" not found');
-    // });
+      expect(result.status).toBe(401);
+      expect(result.text).toBe('User with email address "invalidemailaddress@admin.com" not found');
+      done();
+    });
   });
 });
