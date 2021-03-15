ad3ccf3a6648f2efb261cdc8c053e472d0ce8d63
Jeff Adler <JeffM.Adler@gmail.com>
2021-03-14 20:15:43 -0400

    wrote additional profile routes

---
+++ src/components/profile/tests/profile.test.ts
@@ -0,0 +1,29 @@
+import { TestFactory } from '../../../utils/testing/factory';
+
+describe('Test Profile component', () => {
+  let factory: TestFactory = new TestFactory();
+
+  let token: string;
+
+  beforeAll(async (done) => {
+    await factory.init();
+
+    const result = await factory.app.post('/auth/login').send({
+      email: 'admin@admin.com',
+      password: 'admin_password',
+    });
+
+    token = result.body.token;
+
+    done();
+  });
+
+  afterAll(async () => {
+    await factory.close();
+  });
+
+  describe('GET /profile/', () => {});
+  describe('PATCH /profile/profile/update', () => {});
+  describe('PATCH /profile/change-password', () => {});
+  describe('DELETE /profile/delete', () => {});
+});
