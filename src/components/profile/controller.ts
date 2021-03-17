c30a2a4c80e1a4d697346f7ef1ebbda34a7e6e67
Jeff Adler <JeffM.Adler@gmail.com>
2021-03-16 21:03:40 -0400

    added Response as return type to auth and profile routes

--- src/components/profile/controller.ts
+++ src/components/profile/controller.ts
@@ -9,18 +9,18 @@ export class ProfileController {
   readonly repo: Repository<User> = getConnection(process.env.CONNECTION_TYPE).getRepository(User);
 
   @bind
-  public getProfile(req: Request, res: Response): void {
+  public getProfile(req: Request, res: Response): Response {
     const { id, email, role } = res.locals.currentUser as User;
 
     try {
-      res.status(200).send({ id, email, role });
+      return res.status(200).send({ id, email, role });
     } catch (error) {
-      res.status(404).send('Profile not found');
+      return res.status(404).send('Profile not found');
     }
   }
 
   @bind
-  public async updateProfile(req: Request, res: Response): Promise<void> {
+  public async updateProfile(req: Request, res: Response): Promise<Response> {
     const user: User = res.locals.currentUser;
 
     const updates: string[] = Object.keys(req.body);
@@ -31,7 +31,7 @@ export class ProfileController {
     };
 
     if (!updates.every((update) => isUserField(update))) {
-      res.status(400).send({ error: 'Invalid updates' });
+      return res.status(400).send({ error: 'Invalid updates' });
     }
 
     // Validation 2: fields on req.body are updateable User fields: for this route, only 'email' can be updated
@@ -40,7 +40,7 @@ export class ProfileController {
     };
 
     if (!updates.every((update) => isUpdateableUserField(update))) {
-      res.status(400).send({ error: 'Field cannot be updated' });
+      return res.status(400).send({ error: 'Field cannot be updated' });
     }
 
     // Validation 3: requested updates pass model validations
@@ -50,7 +50,7 @@ export class ProfileController {
     });
     const errors = await validate(user);
     if (errors.length > 0) {
-      res.status(405).send(errors);
+      return res.status(405).send(errors);
     }
 
     // Validation 4: requested updates pass database validations (e.g. email uniqueness)
@@ -59,19 +59,19 @@ export class ProfileController {
       const userObj: User = await this.repo.findOneOrFail(user.id, {
         select: ['id', 'email', 'role'],
       });
-      res.status(204).send(userObj);
+      return res.status(204).send(userObj);
     } catch (e) {
-      res.status(409).send('email already in use');
+      return res.status(409).send('email already in use');
     }
   }
 
   @bind
-  public async changePassword(req: Request, res: Response) {
+  public async changePassword(req: Request, res: Response): Promise<Response> {
     const user: User = res.locals.currentUser;
 
     const { oldPassword, newPassword } = req.body;
     if (!(oldPassword && newPassword)) {
-      res.status(400).send();
+      return res.status(400).send();
     }
 
     if (!user.checkIfUnencryptedPasswordIsValid(oldPassword)) res.status(401).send();
@@ -80,26 +80,26 @@ export class ProfileController {
 
     const errors: ValidationError[] = await validate(user);
     if (errors.length > 0) {
-      res.status(400).send(errors);
+      return res.status(400).send(errors);
     }
 
     try {
       await this.repo.save(user);
-      res.status(204).send('Password changed');
+      return res.status(204).send('Password changed');
     } catch (e) {
-      res.status(401).send(e);
+      return res.status(401).send(e);
     }
   }
 
   @bind
-  public async deleteProfile(req: Request, res: Response): Promise<void> {
+  public async deleteProfile(req: Request, res: Response): Promise<Response> {
     const user: User = res.locals.currentUser;
 
     try {
       await this.repo.delete(user);
-      res.status(200).send('Account deleted');
+      return res.status(200).send('Account deleted');
     } catch (e) {
-      res.status(401).send(e);
+      return res.status(401).send(e);
     }
   }
 }
