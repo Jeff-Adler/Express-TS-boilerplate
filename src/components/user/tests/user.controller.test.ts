afd0ce8d8b7c4e56f5fd71af01ebfd121d9244ff
Jeff Adler <JeffM.Adler@gmail.com>
2021-03-26 23:13:47 -0400

    create user.controller file

---
+++ src/components/user/tests/user.controller.test.ts
@@ -0,0 +1,8 @@
+import { getConnection } from 'typeorm';
+
+import { TestFactory } from '../../../utils/testing/factory';
+import { User } from '../model';
+
+describe('Testing User controller', () => {
+  test.todo('listAll()');
+});
