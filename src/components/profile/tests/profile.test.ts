304c3c302a9cd186e4ecccc5729a53df61af349d
Jeff Adler <JeffM.Adler@gmail.com>
2021-03-28 18:21:13 -0400

    more patch tests

--- src/components/profile/tests/profile.test.ts
+++ src/components/profile/tests/profile.test.ts
@@ -42,9 +42,6 @@ describe('Test Profile component', () => {
   });
   describe('PATCH /profile/update', () => {
     test('Patches permitted fields: email', async (done) => {
-      // const
-      // const seededUser: User = await factory.seedSingleUser();
-
       const newEmail = 'patchedEmail@test.com';
 
       const result = await factory.app
@@ -72,99 +69,96 @@ describe('Test Profile component', () => {
 
       expect(result.status).toBe(201);
 
-      const patchedUser: User = await getConnection(process.env.CONNECTION_TYPE)
-        .getRepository(User)
-        .findOneOrFail({ email: seededUser.email });
+      // TODO: Test that user can no longer login with old password
 
-      const hashedPatchedPassword = patchedUser.password;
-      // Expect that updated password is hashed
-      expect(hashedPatchedPassword).not.toBe(newPassword);
-      // Expect that updated password is not equal to original password
-      expect(hashedPatchedPassword).not.toEqual(seededUser.password);
+      // TODO: Test user can sign in with new password
+
+      // Revert back to original password to be able to sign in with seeded user credentials for other tests
+      const originalPassword = 'admin_password';
 
       done();
     });
 
-    test('Patches permitted fields: role', async (done) => {
-      const seededUser: User = await factory.seedSingleUser();
+    // test('Patches permitted fields: role', async (done) => {
+    //   const seededUser: User = await factory.seedSingleUser();
 
-      const newRole = 'ADMIN';
+    //   const newRole = 'ADMIN';
 
-      const result = await factory.app
-        .patch(`/users/${seededUser.id}`)
-        .send({ role: newRole })
-        .set({ Authorization: `Bearer ${token}` });
+    //   const result = await factory.app
+    //     .patch(`/users/${seededUser.id}`)
+    //     .send({ role: newRole })
+    //     .set({ Authorization: `Bearer ${token}` });
 
-      expect(result.status).toBe(201);
-      expect(result.body.role).toBe('ADMIN');
+    //   expect(result.status).toBe(201);
+    //   expect(result.body.role).toBe('ADMIN');
 
-      const patchedUser: User = await getConnection(process.env.CONNECTION_TYPE)
-        .getRepository(User)
-        .findOneOrFail({ email: seededUser.email });
+    //   const patchedUser: User = await getConnection(process.env.CONNECTION_TYPE)
+    //     .getRepository(User)
+    //     .findOneOrFail({ email: seededUser.email });
 
-      expect(patchedUser.role).toBe('ADMIN');
-      expect(patchedUser.role).not.toBe('USER');
+    //   expect(patchedUser.role).toBe('ADMIN');
+    //   expect(patchedUser.role).not.toBe('USER');
 
-      done();
-    });
+    //   done();
+    // });
 
-    test('Does not patch prohibited fields: id', async (done) => {
-      const seededUser: User = await factory.seedSingleUser();
+    // test('Does not patch prohibited fields: id', async (done) => {
+    //   const seededUser: User = await factory.seedSingleUser();
 
-      //generate Id that is not currently used, to ensure is not due to Id already existing in db
-      const newId = (await getOneMaxId()) + 1;
+    //   //generate Id that is not currently used, to ensure is not due to Id already existing in db
+    //   const newId = (await getOneMaxId()) + 1;
 
-      const postPatchResult = await factory.app
-        .patch(`/users/${seededUser.id}`)
-        .send({ id: newId })
-        .set({ Authorization: `Bearer ${token}` });
+    //   const postPatchResult = await factory.app
+    //     .patch(`/users/${seededUser.id}`)
+    //     .send({ id: newId })
+    //     .set({ Authorization: `Bearer ${token}` });
 
-      expect(postPatchResult.status).toBe(400);
-      expect(postPatchResult.text).toEqual('Field cannot be updated');
+    //   expect(postPatchResult.status).toBe(400);
+    //   expect(postPatchResult.text).toEqual('Field cannot be updated');
 
-      const patchedUser: User = await getConnection(process.env.CONNECTION_TYPE)
-        .getRepository(User)
-        .findOneOrFail({ email: seededUser.email });
+    //   const patchedUser: User = await getConnection(process.env.CONNECTION_TYPE)
+    //     .getRepository(User)
+    //     .findOneOrFail({ email: seededUser.email });
 
-      expect(patchedUser.id).not.toEqual(newId);
-      expect(patchedUser.id).toEqual(seededUser.id);
+    //   expect(patchedUser.id).not.toEqual(newId);
+    //   expect(patchedUser.id).toEqual(seededUser.id);
 
-      done();
-    });
+    //   done();
+    // });
 
-    test('Does not patch prohibited fields: nonExistentField', async (done) => {
-      const seededUser: User = await factory.seedSingleUser();
+    // test('Does not patch prohibited fields: nonExistentField', async (done) => {
+    //   const seededUser: User = await factory.seedSingleUser();
 
-      const newInvalidField = 'invalid update';
+    //   const newInvalidField = 'invalid update';
 
-      const result = await factory.app
-        .patch(`/users/${seededUser.id}`)
-        .send({ newInvalidField })
-        .set({ Authorization: `Bearer ${token}` });
+    //   const result = await factory.app
+    //     .patch(`/users/${seededUser.id}`)
+    //     .send({ newInvalidField })
+    //     .set({ Authorization: `Bearer ${token}` });
 
-      expect(result.status).toBe(400);
-      expect(result.text).toEqual('Field cannot be updated');
+    //   expect(result.status).toBe(400);
+    //   expect(result.text).toEqual('Field cannot be updated');
 
-      done();
-    });
+    //   done();
+    // });
 
-    test('Return 409 if requested email already exists in the db', async (done) => {
-      const seededUser: User = await factory.seedSingleUser();
+    // test('Return 409 if requested email already exists in the db', async (done) => {
+    //   const seededUser: User = await factory.seedSingleUser();
 
-      const users: User[] = await getConnection(process.env.CONNECTION_TYPE).getRepository(User).find({ role: 'USER' });
-      // get random user from db
-      const retrievedUser: User = users[Math.floor(Math.random() * users.length)];
+    //   const users: User[] = await getConnection(process.env.CONNECTION_TYPE).getRepository(User).find({ role: 'USER' });
+    //   // get random user from db
+    //   const retrievedUser: User = users[Math.floor(Math.random() * users.length)];
 
-      const result = await factory.app
-        .patch(`/users/${seededUser.id}`)
-        .send({ email: retrievedUser.email })
-        .set({ Authorization: `Bearer ${token}` });
+    //   const result = await factory.app
+    //     .patch(`/users/${seededUser.id}`)
+    //     .send({ email: retrievedUser.email })
+    //     .set({ Authorization: `Bearer ${token}` });
 
-      expect(result.status).toBe(409);
-      expect(result.text).toBe('email already in use');
+    //   expect(result.status).toBe(409);
+    //   expect(result.text).toBe('email already in use');
 
-      done();
-    });
+    //   done();
+    // });
   });
   // Make sure this does not get blocked by email uniqueness validation
   describe('PATCH /profile/change-password', () => {});
