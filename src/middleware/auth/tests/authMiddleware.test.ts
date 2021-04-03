bd1bd939b551fda7707b069c9bf21647803f448f
Jeff Adler <JeffM.Adler@gmail.com>
2021-04-03 19:57:26 -0400

    setup initial tests for isAuthorized

--- src/middleware/auth/tests/authMiddleware.test.ts
+++ src/middleware/auth/tests/authMiddleware.test.ts
@@ -8,8 +8,20 @@ import { hasPermission } from '../hasPermission';
 describe('Testing Authentication middleware', () => {
   let factory: TestFactory = new TestFactory();
 
+  // Partial keyword lets us not have to worry about mocking every aspect of Request/Response objects
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let mockNext: NextFunction = jest.fn();
+
   beforeAll(async (done) => {
     await factory.init();
+
+    // lets us turn mockRequest into an empty object
+    mockRequest = {};
+    // jset.fn allows us to say we will return a json, but the function that invokes it is just an empty function
+    mockResponse = {
+      json: jest.fn(),
+    };
 
     done();
   });
@@ -33,8 +45,41 @@ describe('Testing Authentication middleware', () => {
 
     test.todo('Sets user to res.locals in response header if valid credentials are sent');
 
-    test.todo('Proceeds to next middleware if valid credentials are sent');
+    test('Proceeds to next middleware if valid credentials are sent', async (done) => {
+      // const mockRequest: any = {
+      //   body: {
+      //     firstName: 'J',
+      //     lastName: 'Doe',
+      //     email: 'jdoe@abc123.com',
+      //     password: 'Abcd1234',
+      //     passwordConfirm: 'Abcd1234',
+      //     company: 'ABC Inc.',
+      //   },
+      // };
 
-    test.todo('Returns 401 status is invalid credentials are sent');
+      // const mockResponse: any = {
+      //   json: jest.fn(),
+      //   status: jest.fn(),
+      // };
+
+      // const mockNext: NextFunction = jest.fn();
+
+      // await userRegister(mockRequest, mockResponse, mockNext);
+
+      // this should be the expected return value:
+      expect(mockNext).toHaveBeenCalledTimes(1);
+
+      done();
+    });
+
+    test('Returns 401 status is invalid credentials are sent', async (done) => {
+      const expectedResponse = 'Authentication Failed';
+
+      isAuthorized(mockRequest as Request, mockResponse as Response, mockNext);
+      console.log(mockResponse.json);
+      expect(mockResponse.json).toBeCalledWith(expectedResponse);
+
+      done();
+    });
   });
 });
