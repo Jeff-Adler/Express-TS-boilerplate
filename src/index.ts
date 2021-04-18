5d991338d8e602a3b9e974248eb7f14a242584a9
Jeff Adler <JeffM.Adler@gmail.com>
2021-04-18 19:50:15 -0400

    setup winston logger system

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,27 @@ import { createConnection } from 'typeorm';
 import express from 'express';
 import { App } from './app';
 
+import { Logger } from './lib/logger';
+
 (async function main() {
   try {
     // Initialize database connection
-    console.log('Initializing ORM connection...');
+    Logger.debug('Initializing ORM connection...');
     const connection = await createConnection(`${process.env.CONNECTION_TYPE}`);
 
     // Initialize Express app
     const app: express.Application = new App().app;
 
+    Logger.error('This is an error log');
+    Logger.warn('This is a warn log');
+    Logger.info('This is a info log');
+    Logger.http('This is a http log');
+    Logger.debug('This is a debug log');
+
     // Initialize Server
     const PORT = process.env.PORT || 8080;
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      Logger.debug(`Server is running on port ${PORT}`);
     });
   } catch (err) {
     console.log(err);
